243b01e462473b6a19ff9bba637038b1d6370ed9
Nahyun Kim <hedghog1004@naver.com>
2024-03-01 03:13:22 +0900

    ✨ Feat: Codelab 단원4 과정3 - 적응형 navigation 레이아웃 구현
    Reply
    동적 navigation으로 적응형 앱 빌드
    - navigationType 추가
    - 다양한 화면에서의 navigation 위치 조정

--- Reply/app/src/main/java/com/example/reply/ui/ReplyApp.kt
+++ Reply/app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -22,27 +22,34 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.reply.data.Email
 import com.example.reply.data.MailboxType
+import com.example.reply.ui.utils.ReplyNavigationType
 
 @Composable
 fun ReplyApp(
     windowSize: WindowWidthSizeClass,
     modifier: Modifier = Modifier,
 ) {
+    val navigationType: ReplyNavigationType
     val viewModel: ReplyViewModel = viewModel()
     val replyUiState = viewModel.uiState.collectAsState().value
 
     when (windowSize) {
         WindowWidthSizeClass.Compact -> {
+            navigationType = ReplyNavigationType.BOTTOM_NAVIGATION
         }
         WindowWidthSizeClass.Medium -> {
+            navigationType = ReplyNavigationType.NAVIGATION_RAIL
         }
         WindowWidthSizeClass.Expanded -> {
+            navigationType = ReplyNavigationType.PERMANENT_NAVIGATION_DRAWER
         }
         else -> {
+            navigationType = ReplyNavigationType.BOTTOM_NAVIGATION
         }
     }
 
     ReplyHomeScreen(
+        navigationType = navigationType,
         replyUiState = replyUiState,
         onTabPressed = { mailboxType: MailboxType ->
             viewModel.updateCurrentMailbox(mailboxType = mailboxType)
