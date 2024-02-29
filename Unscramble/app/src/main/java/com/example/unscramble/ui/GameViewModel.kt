e4f47130e49021d142fd5a65894cd0fd635aa693
Nahyun Kim <hedghog1004@naver.com>
2024-02-29 18:18:39 +0900

    ✨ Feat: Codelab 단원4 과정1 - ViewModel 추가
    Unscramble
    Compose의 ViewModel 및 상태
    - 뷰모델 의존성 추가
    - StateFlow 사용
    - 여러 게임 메서드 구현

---
+++ Unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.unscramble.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class GameViewModel : ViewModel() {
+    // Game UI state
+
+    // Backing property to avoid state updates from other classes
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+    // Set of words used in the game
+    private var usedWords: MutableSet<String> = mutableSetOf() // 게임에서 사용된 단어를 저장
+    private lateinit var currentWord: String // 글자가 현재 섞인 현재 단어를 저장
+
+    init {
+        resetGame()
+    }
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+
+    private fun pickRandomWordAndShuffle(): String {
+        // Continue picking up a new random word until you get one that hasn't been used before
+        currentWord = allWords.random()
+        if (usedWords.contains(currentWord)) {
+            return pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        // Scramble the word
+        tempWord.shuffle()
+        while (String(tempWord).equals(word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+}
