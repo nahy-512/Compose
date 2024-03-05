08340e419d98a9eb987f621db6a1cb244a2511ae
Nahyun Kim <hedghog1004@naver.com>
2024-03-06 01:39:09 +0900

    ✅ Test: Codelab 단원5 과정1 - 단위 테스트를 작성하여 코루틴 테스트
    Race Tracker
    레이스를 시작하고 완료하기 위한 단위 테스트 구현
    - 레이스 시작 후 진행률
    - 레이스 완료 후 진행률

--- Race Tracker/app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
+++ Race Tracker/app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
@@ -16,6 +16,12 @@
 package com.example.racetracker
 
 import com.example.racetracker.ui.RaceParticipant
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
 
 class RaceParticipantTest {
     private val raceParticipant = RaceParticipant(
@@ -25,4 +31,23 @@ class RaceParticipantTest {
         initialProgress = 0,
         progressIncrement = 1
     )
+
+    /** 레이스 시작 후 진행률 */
+    @Test
+    fun raceParticipant_RaceStarted_ProgressUpdated() = runTest {
+        val expectedProgress = 1
+        launch { raceParticipant.run() } // 레이스 시작
+        advanceTimeBy(raceParticipant.progressDelayMillis) // 레이스 진행률이 업데이트되기 전에 걸리는 시간 결정
+        runCurrent() // 현재 시간에 대기 중인 작업 실행
+        assertEquals(expectedProgress, raceParticipant.currentProgress) // 진행률 업데이트 확인
+    }
+
+    /** 레이스 완료 후 진행률 */
+    @Test
+    fun raceParticipant_RaceFinished_ProgressUpdated() = runTest {
+        launch { raceParticipant.run() }
+        advanceTimeBy(raceParticipant.maxProgress * raceParticipant.progressDelayMillis) // 레이스 완료
+        runCurrent() // 대기 중인 작업 실행
+        assertEquals(100, raceParticipant.currentProgress) // 완료 후 진행률이 올바르게 되었는지 확인
+    }
 }
