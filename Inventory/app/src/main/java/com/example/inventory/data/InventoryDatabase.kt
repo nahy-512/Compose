3b9b4816d43af6dbc6772409f9e2828ea0a31ebd
Nahyun Kim <hedghog1004@naver.com>
2024-03-15 02:51:44 +0900

    ✨ Feat: Codelab 단원6 과정2 - 데이터베이스 인스턴스 만들기
    Inventory
    - 이전 작업의 Entity 및 DAO를 사용하는 RoomDatabase
    - Database 클래스는 정의된 DAO의 인스턴스를 앱에 제공

---
+++ Inventory/app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -0,0 +1,26 @@
+package com.example.inventory.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Item::class], version = 1, exportSchema = false)
+abstract class InventoryDatabase : RoomDatabase() {
+    abstract fun itemDao(): ItemDao
+
+    companion object {
+        @Volatile
+        private var Instance: InventoryDatabase? = null
+
+        fun getDatabase(context: Context): InventoryDatabase {
+            // if the Instance is not null, return it, otherwise create a new database instance.
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { Instance = it } // 최근에 만들어진 db 인스턴스에 대한 참조를 유지
+            }
+        }
+    }
+}
