5861df30d2bbc3eb8cbd2d86641f4611c0cb60c3
Nahyun Kim <hedghog1004@naver.com>
2024-03-15 11:49:36 +0900

    ✨ Feat: Codelab 단원6 과정2 - 항목 삭제 기능 구현
    Inventory
    - 테스트 코드 작성
    - showConfirmationDialog() 확인 버튼 클릭 시 삭제
    - 삭제 후 뒤로가기

--- Inventory/app/src/androidTest/kotlin/ItemDaoTest.kt
+++ Inventory/app/src/androidTest/kotlin/ItemDaoTest.kt
@@ -6,6 +6,7 @@ import com.example.inventory.data.InventoryDatabase
 import com.example.inventory.data.Item
 import com.example.inventory.data.ItemDao
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.junit.After
@@ -67,6 +68,16 @@ class ItemDaoTest {
         val allItems = itemDao.getAllItems().first()
         assertEquals(allItems[0], Item(1, "Apples", 15.0, 25))
         assertEquals(allItems[1], Item(2, "Bananas", 5.0, 50))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun daoDeleteItems_deletesAllItemsFromDB() = runBlocking {
+        addTwoItemsToDb()
+        itemDao.delete(item1)
+        itemDao.delete(item2)
+        val allItems = itemDao.getAllItems().first()
+        assertTrue(allItems.isEmpty())
     }
 
     private suspend fun addOneItemToDb() {
